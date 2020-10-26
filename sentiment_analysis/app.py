ff7008895650111c0067bb1120f49f3706bb2cb9
Gkrumbach07 <gkrumbac@redhat.com>
2020-10-26 17:31:15 -0500

    fix

--- sentiment_analysis/app.py
+++ sentiment_analysis/app.py
@@ -1,36 +1,61 @@
-import flair
-import kafka
+import argparse
 import logging
-import json
-from optparse import OptionParser
-
-DEFAULT_TOPIC = "decoded_speech"
-DEFAULT_BROKER = "localhost:9020"
-
-parser.add_option ("-t", "--topic", dest="topic", type = "string", default=DEFAULT_TOPIC,
-                    help="kafka topic, default: %d" % DEFAULT_TOPIC)
-
-parser.add_option ("-b", "--broker", dest="broker", type = "string", default=DEFAULT_BROKER,
-                    help="kafka broker, default: %d" % DEFAULT_BROKER)
-
-(options, args) = parser.parse_args()
-logging.basicConfig(level=logging.INFO)
-
-flair_sentiment = flair.models.TextClassifier.load('en-sentiment')
-s = flair.data.Sentence("This is a test and I am not happy.")
-flair_sentiment.predict(s)
-total_sentiment = s.labels
+import os
+import flair
+from kafka import KafkaConsumer
 
 
-logging.info('starting kafka consumer')
-    consumer = kafka.KafkaConsumer(args.topic, bootstrap_servers=args.brokers)
-    while(True):
-        for msg in consumer:
-            try:
-                message = json.loads(str(msg.value, 'utf-8'))
-                if message == 'exit':
-                    break
-                logging.info(message)
-            except Exception as e:
-                logging.error(e.message)
-        logging.info('exiting kafka consumer')
+def get_arg(env, default):
+    return os.getenv(env) if os.getenv(env, '') is not '' else default
+
+
+def parse_args(parser):
+    args = parser.parse_args()
+    args.brokers = get_arg('KAFKA_BROKERS', args.brokers)
+    args.topic = get_arg('KAFKA_TOPIC', args.topic)
+    return args
+
+
+def main(args):
+    flair_sentiment = flair.models.TextClassifier.load('en-sentiment')
+    s = flair.data.Sentence("This is a test and I am not happy.")
+    flair_sentiment.predict(s)
+    total_sentiment = s.labels
+
+    consumer = KafkaConsumer(args.topic, bootstrap_servers=args.brokers)
+    for msg in consumer:
+        out = msg.value if msg.value is not None else ""
+        logging.info('received: ' + str(msg.value, 'utf-8'))
+    logging.info('exiting')
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    logging.info('starting kafka-python-listener')
+    parser = argparse.ArgumentParser(
+            description='listen for some stuff on kafka')
+    parser.add_argument(
+            '--brokers',
+            help='The bootstrap servers, env variable KAFKA_BROKERS',
+            default='localhost:9092')
+    parser.add_argument(
+            '--topic',
+            help='Topic to publish to, env variable KAFKA_TOPIC',
+            default='bones-brigade')
+    args = parse_args(parser)
+    main(args)
+
+
+
+# logging.info('starting kafka consumer')
+#     consumer = kafka.KafkaConsumer(args.topic, bootstrap_servers=args.brokers)
+#     while(True):
+#         for msg in consumer:
+#             try:
+#                 message = json.loads(str(msg.value, 'utf-8'))
+#                 if message == 'exit':
+#                     break
+#                 logging.info(message)
+#             except Exception as e:
+#                 logging.error(e.message)
+#         logging.info('exiting kafka consumer')
