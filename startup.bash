777abe81a426394c3a9524456e860957817046b9
Gkrumbach07 <gkrumbac@redhat.com>
2021-01-15 11:41:38 -0600

    added key

--- startup.bash
+++ startup.bash
@@ -6,13 +6,13 @@ oc new-project audio-decoder-demo
 oc project audio-decoder-demo
 
 # kafka consumer for decoded speech -> pushes data through sentiment analysis
-oc new-app registry.access.redhat.com/ubi8/python-36~https://github.com/Gkrumbach07/docker-py-kaldi-asr.git \
-  -e KAFKA_BROKERS=$BROKER:9092 \
-  -e KAFKA_TOPIC=decoded-speech \
-  --name=sentiment-consumer \
-  --context-dir=sentiment_analysis
-
-oc expose service/sentiment-consumer
+# oc new-app registry.access.redhat.com/ubi8/python-36~https://github.com/Gkrumbach07/docker-py-kaldi-asr.git \
+#   -e KAFKA_BROKERS=$BROKER:9092 \
+#   -e KAFKA_TOPIC=decoded-speech \
+#   --name=sentiment-consumer \
+#   --context-dir=sentiment_analysis
+#
+# oc expose service/sentiment-consumer
 
 # audio decoder multi user api
 oc new-app \
@@ -21,13 +21,20 @@ oc new-app \
 
 oc expose service/audio-decoder
 
+HOST=$(oc get routes/audio-decoder -o jsonpath='{ .status.ingress[0].routerCanonicalHostname }')
+
 # change to use multi user
 
 
 oc new-app registry.access.redhat.com/ubi8/python-36~https://github.com/Gkrumbach07/docker-py-kaldi-asr.git \
   -e KAFKA_BROKERS=$BROKER:9092 \
   -e KAFKA_TOPIC=decoded-speech \
-  -e HOST=audio-decoder-audio-decoder-demo.apps.audioclusterdemo.lab.pnq2.cee.redhat.com \
+  -e HOST=$HOST \
   -e DO_SIMULATE=True \
   --name=call-simulator \
   --context-dir=recorder
+
+
+oc create -f https://raw.githubusercontent.com/jupyter-on-openshift/jupyter-notebooks/master/image-streams/s2i-minimal-notebook.json
+oc new-app s2i-minimal-notebook:3.6 --name minimal-notebook
+oc expose svc/minimal-notebook
